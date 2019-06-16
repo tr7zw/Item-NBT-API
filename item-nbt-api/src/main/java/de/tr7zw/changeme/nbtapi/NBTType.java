5d31541e68f11efffcc86c96281e04a0b369e9d2
tr7zw <tr7zw@live.de>
2019-06-16 18:03:23 +0200

    Fix

--- item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/NBTType.java
+++ item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/NBTType.java
@@ -10,16 +10,6 @@ package de.tr7zw.changeme.nbtapi;
 public enum NBTType {
 	NBTTagEnd(0),
     NBTTagByte(1),
-    NBTTagShort(2),
-    NBTTagInt(3),
-    NBTTagLong(4),
-    NBTTagFloat(5),
-    NBTTagDouble(6),
-    NBTTagByteArray(7),
-    NBTTagIntArray(11),
-    NBTTagString(8),
-    NBTTagList(9),
-    NBTTagCompound(10);
 	NBTTagShort(2), 
 	NBTTagInt(3), 
 	NBTTagLong(4), 
