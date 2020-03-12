fd9bbe093b1e1821fec5d4133963635e724ebda1
tr7zw <tr7zw@live.de>
2020-03-12 22:12:09 +0100

    Add Equals unittest

--- item-nbt-plugin/src/main/java/de/tr7zw/nbtapi/plugin/NBTAPI.java
+++ item-nbt-plugin/src/main/java/de/tr7zw/nbtapi/plugin/NBTAPI.java
@@ -14,6 +14,7 @@ import de.tr7zw.changeme.nbtapi.utils.nmsmappings.ClassWrapper;
 import de.tr7zw.changeme.nbtapi.utils.nmsmappings.ReflectionMethod;
 import de.tr7zw.nbtapi.plugin.tests.GameprofileTest;
 import de.tr7zw.nbtapi.plugin.tests.NBTFileTest;
+import de.tr7zw.nbtapi.plugin.tests.compounds.EqualsTest;
 import de.tr7zw.nbtapi.plugin.tests.compounds.ForEachTest;
 import de.tr7zw.nbtapi.plugin.tests.compounds.GetterSetterTest;
 import de.tr7zw.nbtapi.plugin.tests.compounds.ListTest;
@@ -67,6 +68,7 @@ public class NBTAPI extends JavaPlugin {
 		apiTests.add(new MergeTest());
 		apiTests.add(new ForEachTest());
 		apiTests.add(new StreamTest());
+		apiTests.add(new EqualsTest());
 
 		// Items
 		apiTests.add(new ItemConvertionTest());
---
+++ item-nbt-plugin/src/main/java/de/tr7zw/nbtapi/plugin/tests/compounds/EqualsTest.java
@@ -0,0 +1,32 @@
+package de.tr7zw.nbtapi.plugin.tests.compounds;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import de.tr7zw.changeme.nbtapi.NBTCompound;
+import de.tr7zw.changeme.nbtapi.NBTContainer;
+import de.tr7zw.changeme.nbtapi.NBTItem;
+import de.tr7zw.changeme.nbtapi.NbtApiException;
+import de.tr7zw.nbtapi.plugin.tests.Test;
+
+public class EqualsTest implements Test{
+
+	@Override
+	public void test() throws Exception {
+		NBTContainer cont = new NBTContainer();
+		cont.setString("hello", "world");
+		cont.setInteger("theAnswer", 42);
+		cont.addCompound("sub").setString("me", "too");
+		ItemStack item = new ItemStack(Material.STONE);
+		NBTItem nbti = new NBTItem(item);
+		NBTCompound customData = nbti.addCompound("customData");
+		// reverse order
+		customData.addCompound("sub").setString("me", "too");
+		customData.setInteger("theAnswer", 42);
+		customData.setString("hello", "world");
+		if(!customData.equals(cont)) {
+			throw new NbtApiException("Compounds did not match! " + customData + " " + cont);
+		}
+	}
+
+}
