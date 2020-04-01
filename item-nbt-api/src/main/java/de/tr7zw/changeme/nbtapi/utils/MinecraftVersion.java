05da1c6b1c2343cc4cb753ea1a3714b6fa8bb754
tr7zw <tr7zw@live.de>
2020-04-02 01:18:13 +0200

    Bump version for tag

--- item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/MinecraftVersion.java
+++ item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/MinecraftVersion.java
@@ -39,7 +39,7 @@ public enum MinecraftVersion {
 	public static final Logger logger = Logger.getLogger("NBTAPI");
 	
 	// NBT-API Version
-	protected static final String VERSION = "2.3.0-SNAPSHOT";
+	protected static final String VERSION = "2.3.0";
 
 	private final int versionId;
 
