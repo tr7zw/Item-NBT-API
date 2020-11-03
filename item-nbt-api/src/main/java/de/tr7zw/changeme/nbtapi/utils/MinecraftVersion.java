7d76d3c342694eb1446370e2df0f357b2dc39930
tr7zw <tr7zw@live.de>
2020-11-03 18:15:20 +0100

    2.6.0 Release

--- item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/MinecraftVersion.java
+++ item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/MinecraftVersion.java
@@ -30,7 +30,7 @@ public enum MinecraftVersion {
 	private static Logger logger = Logger.getLogger("NBTAPI");
 
 	// NBT-API Version
-	protected static final String VERSION = "2.6.0-SNAPSHOT";
+	protected static final String VERSION = "2.6.0";
 
 	private final int versionId;
 
