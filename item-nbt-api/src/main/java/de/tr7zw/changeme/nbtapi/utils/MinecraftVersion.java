a4deb2a1eebea4892596143a27a25dadd06f5a09
tr7zw <tr7zw@live.de>
2020-12-20 18:40:07 +0100

    Release 2.6.1 to fix the spigotmc/curseforge situation

--- item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/MinecraftVersion.java
+++ item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/MinecraftVersion.java
@@ -30,7 +30,7 @@ public enum MinecraftVersion {
 	private static Logger logger = Logger.getLogger("NBTAPI");
 
 	// NBT-API Version
-	protected static final String VERSION = "2.6.1-SNAPSHOT";
+	protected static final String VERSION = "2.6.1";
 
 	private final int versionId;
 
