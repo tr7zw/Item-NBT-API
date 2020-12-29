a40eb5b4b4e64c76af8a8d96161a5bb2cbe0f817
tr7zw <tr7zw@live.de>
2020-12-29 16:51:30 +0100

    Start work on 2.7.0-SNAPSHOT

--- item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/MinecraftVersion.java
+++ item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/MinecraftVersion.java
@@ -30,7 +30,7 @@ public enum MinecraftVersion {
 	private static Logger logger = Logger.getLogger("NBTAPI");
 
 	// NBT-API Version
-	protected static final String VERSION = "2.6.1";
+	protected static final String VERSION = "2.7.0-SNAPSHOT";
 
 	private final int versionId;
 
