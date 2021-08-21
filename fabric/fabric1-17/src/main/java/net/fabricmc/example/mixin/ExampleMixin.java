a26eb4d72521bdad8e8d6878da9aa387d6ea3b1a
Tofik ♡ <62406295+Toffikk@users.noreply.github.com>
2021-08-21 12:20:58 +0200

    Update ExampleMixin.java

--- fabric/fabric1-17/src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ fabric/fabric1-17/src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
 package net.fabricmc.example.mixin;
 
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
