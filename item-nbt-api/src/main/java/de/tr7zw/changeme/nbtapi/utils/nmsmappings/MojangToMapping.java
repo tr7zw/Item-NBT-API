20f5787696d3fa2749e8726d0c53f08204af2006
tr7zw <tr7zw@live.de>
2022-06-07 19:19:42 +0200

    Add 1.19(.0) support.

--- item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/nmsmappings/MojangToMapping.java
+++ item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/nmsmappings/MojangToMapping.java
@@ -86,11 +86,24 @@ public class MojangToMapping {
         }
     };
     
+    @SuppressWarnings("serial")
+    private static Map<String, String> MC1_19R1 = new HashMap<String, String>() {
+      
+        {
+            putAll(MC1_18R2);
+            
+            put("net.minecraft.world.item.ItemStack#getTag()", "u");
+        }
+        
+    };
+    
+    
     public static Map<String, String> getMapping(){
         switch(MinecraftVersion.getVersion()) {
+        case MC1_19_R1: return MC1_19R1;
         case MC1_18_R2: return MC1_18R2;
         case MC1_18_R1: return MC1_18R1;
-        default: return MC1_18R2;//throw new NbtApiException("This version of the NBTAPI is not compatible with this server version!");
+        default: return MC1_19R1;//throw new NbtApiException("This version of the NBTAPI is not compatible with this server version!");
         }
     }
     
