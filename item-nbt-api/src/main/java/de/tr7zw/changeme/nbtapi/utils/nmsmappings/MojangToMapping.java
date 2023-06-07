0653f47bc14ca40877bd9e4a7a7b6b4656fb3358
tr7zw <tr7zw@live.de>
2023-06-07 20:14:05 +0200

    Add 1.20 support

--- item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/nmsmappings/MojangToMapping.java
+++ item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/nmsmappings/MojangToMapping.java
@@ -41,7 +41,6 @@ public class MojangToMapping {
             put("net.minecraft.nbt.CompoundTag#getShort(java.lang.String)", "g");
             put("net.minecraft.nbt.CompoundTag#putByte(java.lang.String,byte)", "a");
             put("net.minecraft.nbt.CompoundTag#getAllKeys()", "d");
-            put("net.minecraft.nbt.CompoundTag#getAllKeys()", "d");
             put("net.minecraft.nbt.CompoundTag#putUUID(java.lang.String,java.util.UUID)", "a");
             put("net.minecraft.nbt.CompoundTag#putShort(java.lang.String,short)", "a");
             put("net.minecraft.nbt.CompoundTag#getLong(java.lang.String)", "i");
@@ -108,8 +107,22 @@ public class MojangToMapping {
 
     };
 
+    @SuppressWarnings("serial")
+    private static Map<String, String> MC1_20R1 = new HashMap<String, String>() {
+
+        {
+            putAll(MC1_19R2);
+
+            put("net.minecraft.world.entity.Entity#getEncodeId()", "br");
+            put("net.minecraft.world.item.ItemStack#getTag()", "v");
+        }
+
+    };
+
     public static Map<String, String> getMapping() {
         switch (MinecraftVersion.getVersion()) {
+        case MC1_20_R1:
+            return MC1_20R1;
         case MC1_19_R2:
             return MC1_19R2;
         case MC1_19_R1:
