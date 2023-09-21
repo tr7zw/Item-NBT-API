dc333ae25a365fd1a43e7733890423f4c6197e23
tr7zw <tr7zw@live.de>
2023-09-21 23:20:21 +0200

    Release 2.12.0

--- item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/MinecraftVersion.java
+++ item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/MinecraftVersion.java
@@ -36,7 +36,7 @@ public enum MinecraftVersion {
     private static Logger logger = Logger.getLogger("NBTAPI");
 
     // NBT-API Version
-    protected static final String VERSION = "2.12.0-SNAPSHOT";
+    protected static final String VERSION = "2.12.0";
 
     private final int versionId;
     private final boolean mojangMapping;
