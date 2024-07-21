c939105e456f93a295ad374306e79c3e09ebbbb8
tr7zw <tr7zw@live.de>
2024-07-21 20:51:48 +0200

    Start 2.13.2-SNAPSHOT

--- item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/MinecraftVersion.java
+++ item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/MinecraftVersion.java
@@ -39,7 +39,7 @@ public enum MinecraftVersion {
     private static Logger logger = Logger.getLogger("NBTAPI");
 
     // NBT-API Version
-    protected static final String VERSION = "2.13.1";
+    protected static final String VERSION = "2.13.2-SNAPSHOT";
 
     private final int versionId;
     private final boolean mojangMapping;
