bd674e31c05631adc8f20f935ac381a97f5b7715
tr7zw <tr7zw@live.de>
2024-11-02 15:05:58 +0100

    Tag 2.14.0

--- item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/MinecraftVersion.java
+++ item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/MinecraftVersion.java
@@ -39,7 +39,7 @@ public enum MinecraftVersion {
     private static Logger logger = Logger.getLogger("NBTAPI");
 
     // NBT-API Version
-    protected static final String VERSION = "2.13.3-SNAPSHOT";
+    protected static final String VERSION = "2.14.0";
 
     private final int versionId;
     private final boolean mojangMapping;
