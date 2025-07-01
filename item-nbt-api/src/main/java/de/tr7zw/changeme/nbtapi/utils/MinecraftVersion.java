6bcd00d1148c9f8c8fcc0a6652200723e18e1dc8
tr7zw <tr7zw@live.de>
2025-07-01 19:42:39 +0200

    Bump to 2.15.1 release

--- item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/MinecraftVersion.java
+++ item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/MinecraftVersion.java
@@ -48,7 +48,7 @@ public enum MinecraftVersion {
     private static Logger logger = Logger.getLogger("NBTAPI");
 
     // NBT-API Version
-    protected static final String VERSION = "2.15.1-SNAPSHOT";
+    protected static final String VERSION = "2.15.1";
 
     private final int versionId;
     private final boolean mojangMapping;
