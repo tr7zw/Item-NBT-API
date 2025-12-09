22c0b0385bfb3e1d5d2b2061208e00b0c5a3c81a
tr7zw <tr7zw@live.de>
2025-12-09 18:44:30 +0100

    Release 2.15.4

--- item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/MinecraftVersion.java
+++ item-nbt-api/src/main/java/de/tr7zw/changeme/nbtapi/utils/MinecraftVersion.java
@@ -48,7 +48,7 @@ public enum MinecraftVersion {
     private static Logger logger = Logger.getLogger("NBTAPI");
 
     // NBT-API Version
-    protected static final String VERSION = "2.15.4-SNAPSHOT";
+    protected static final String VERSION = "2.15.4";
 
     private final int versionId;
     private final boolean mojangMapping;
